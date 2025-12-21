1e41dabbfb255154b344fbfa94c6e8a54dffa147
Pratikash Panda <pratikashpanda@Mac.alphionsee.in>
2025-12-21 17:56:54 +0530

    configured convex and clerk and created header

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import { ConvexClientProvider } from "@/components/convex-client-provider";
-import { ClerkProvider } from "@clerk/nextjs";
+import { ClerkProvider } from "@clerk/nextjs"; 
 
 const inter = Inter({ subsets: ["latin"] });
 
